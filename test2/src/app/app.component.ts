495cbcb76e0d53a51a4bfd746de3cd5eff8f4b11
Viktor Tishchenko <9043258451vvt@gmail.com>
2023-07-20 10:08:11 +0600

    init: app w bootstrap & navbar

---
+++ test2/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-root",
+  template: `
+    <nav class="navbar navbar-expand bg-light">
+      <div class="container">
+        <div class="container-fluid d-flex flex-wrap fix-position">
+          <a class="navbar-brand" href="#">Angular app</a>
+          <div class="navbar-nav flex-wrap">
+            <a class="nav-link">Home</a>
+            <a class="nav-link">Products</a>
+            <a class="nav-link">About</a>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <div class="container">content</div>
+  `,
+  styles: [],
+})
+export class AppComponent {
+  title = "test2";
+}
