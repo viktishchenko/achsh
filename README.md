3c6f6eb901d42f96d0c8dda6efe7a21e65cab895
Viktor Tishchenko <9043258451vvt@gmail.com>
2023-08-12 21:28:59 +0600

    init: rm-test app w custom theme

---
+++ README.md
@@ -0,0 +1,23 @@
+# r&m test project
+
+- init
+
+<details>
+
+```js
+git switch --orphan test6
+New-Item .gitignore
+New-Item README.md
+mkdir test6
+cd test6
+ng new form --directory ./
+// add material & custom theme
+// custom palette (http://mcg.mbitson.com/#!?mcgpalette0=%23da2032)
+ng add @angular/material
+
+git commit --allow-empty -m "Initial"
+git push -u origin test6
+
+```
+
+</details>
---
+++ test6/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from './angular-material-custom-theme';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
